9a39f2c2e5764d7e99ca5f98e064d997a32aa530
Rafael Leme <rafaelleme_2@hotmail.com>
2020-03-30 21:34:08 -0300

    created classes and repositories for product and carrier

---
+++ src/Domain/Carrier/Carrier.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Domain\Carrier;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Infrastructure\Carrier\Repository\CarrierRepository")
+ */
+class Carrier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $fix_value;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $value_distance_kilo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFixValue(): ?float
+    {
+        return $this->fix_value;
+    }
+
+    public function setFixValue(float $fix_value): self
+    {
+        $this->fix_value = $fix_value;
+
+        return $this;
+    }
+
+    public function getValueDistanceKilo(): ?float
+    {
+        return $this->value_distance_kilo;
+    }
+
+    public function setValueDistanceKilo(float $value_distance_kilo): self
+    {
+        $this->value_distance_kilo = $value_distance_kilo;
+
+        return $this;
+    }
+}
---
+++ src/Domain/Product/Product.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Domain\Product;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Infrastructure\Product\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $weight;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $distance;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getWeight(): ?float
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(float $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getDistance(): ?float
+    {
+        return $this->distance;
+    }
+
+    public function setDistance(float $distance): self
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+}
